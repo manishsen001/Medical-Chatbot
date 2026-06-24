32d44c94238b4b41d773aff5ba398afa20ccf115
Manish Sen <manishsen91522@gmail.com>
2026-06-24 21:48:41 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import  find_packages,setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Manish Sen",
+    author_email="<manishsen91522@gmail.com>",
+    packages =find_packages(),
+    install_requires=[]
+)
